56c9e000985f78375b3bb28b66e903896a2c27b6
Rodrigo Dominguez Peniche <rodrigo.02.93@gmail.com>
2019-11-08 11:22:25 -0600

    Add project files.

---
+++ AutomationTraining_M7/Base Files/BaseTest.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTraining_M7.Base_Files {
+    class BaseTest {
+        public static IWebDriver driver;
+        private static string BrowserName = ConfigurationManager.AppSettings.Get("url");
+
+        public static void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Url = BrowserName;
+        }
+        public static void AfterTest()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+
+        public static void FnSendkeyAndClear(By by, string pstrText)
+        {
+            driver.FindElement(by).Clear();
+            driver.FindElement(by).SendKeys(pstrText);
+        }
+    }
+}
---
+++ AutomationTraining_M7/Program.cs
@@ -0,0 +1,32 @@
+﻿using AutomationTraining_M7.Base_Files;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTraining_M7 {
+    class Program : BaseTest {
+        static void Main(string[] args)
+        {
+            SetUp();
+
+            //driver.FindElement(By.Name("userName")).SendKeys(ConfigurationManager.AppSettings.Get("username"));
+            //driver.FindElement(By.Name("password")).SendKeys(ConfigurationManager.AppSettings.Get("password"));
+
+            FnSendkeyAndClear(By.Name("userName"), ConfigurationManager.AppSettings.Get("username"));
+            FnSendkeyAndClear(By.Name("password"), ConfigurationManager.AppSettings.Get("password"));
+
+            FnSendkeyAndClear(By.XPath("//*[@name='userName']"), ConfigurationManager.AppSettings.Get("username2"));
+            FnSendkeyAndClear(By.XPath("//*[@name='password']"), ConfigurationManager.AppSettings.Get("password2"));
+            driver.FindElement(By.Name("login")).Click();
+
+            AfterTest();
+
+            Console.ReadKey();
+        }
+    }
+}
